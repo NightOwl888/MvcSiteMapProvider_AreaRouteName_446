11212442959ee59252d73fe9abb529afd1708c45
Shad Storhaug <shad@shadstorhaug.com>
2016-06-22 02:15:49 +0700

    Default MVC 5 Visual Studio 2015 Template.

---
+++ MvcSiteMapProvider_AreaRouteName_446/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcSiteMapProvider_AreaRouteName_446.Startup))]
+namespace MvcSiteMapProvider_AreaRouteName_446
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
