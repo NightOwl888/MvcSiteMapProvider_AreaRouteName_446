7821b94c3ede1350ac5c1d573f0a42a7b40c38dc
Shad Storhaug <shad@shadstorhaug.com>
2016-06-22 02:48:28 +0700

    Added Area with Mvc.sitemap route attribute and also a RouteLink to demonstrate similar behavior.

---
+++ MvcSiteMapProvider_AreaRouteName_446/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace MvcSiteMapProvider_AreaRouteName_446.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+
+            // NOTE: This route cannot be accessed through a URL, but using route attribute
+            // the URL can be generated, exactly the same way that RouteLink works.
+            context.MapRoute(
+                "Admin_test",
+                "Admin/Boo/{action}/{id}",
+                new { controller = "Settings", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MvcSiteMapProvider_AreaRouteName_446/Areas/Admin/Controllers/SettingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSiteMapProvider_AreaRouteName_446.Areas.Admin.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Admin/Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
